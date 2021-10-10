1fed86a164572161712222e9ced8a06170da0a09
Kirill_Kabanov@epam.com <Kirill_Kabanov@epam.com>
2021-10-10 12:55:43 +0000

    Is able to read data from input abfs

--- notebooks/streaming_notebook.py
+++ notebooks/streaming_notebook.py
@@ -1,2 +1,47 @@
 # Databricks notebook source
-# TODO
+import configparser
+
+secret = dbutils.secrets.get(scope="abfs-access", key="storage-creds")
+
+config = configparser.ConfigParser()
+config.read_string(secret)
+
+class AZStorage:
+    IN_STORAGE_ACCOUNT = config["INPUT"]["AZ_STORAGE_ACCOUNT"]
+    IN_CONTAINER = config["INPUT"]["AZ_CONTAINER"]
+    IN_CLIENT_ID = config["INPUT"]["AZ_CLIENT_ID"]
+    IN_CLIENT_SECRET = config["INPUT"]["AZ_CLIENT_SECRET"]
+    IN_CLIENT_ENDPOINT = config["INPUT"]["AZ_CLIENT_ENDPOINT"]
+
+    OUT_STORAGE_ACCOUNT = config["OUTPUT"]["AZ_STORAGE_ACCOUNT"]
+    OUT_CONTAINER = config["OUTPUT"]["AZ_CONTAINER"]
+    OUT_CLIENT_ID = config["OUTPUT"]["AZ_CLIENT_ID"]
+    OUT_CLIENT_SECRET = config["OUTPUT"]["AZ_CLIENT_SECRET"]
+    OUT_CLIENT_ENDPOINT = config["OUTPUT"]["AZ_CLIENT_ENDPOINT"]
+
+# COMMAND ----------
+
+spark.conf.set(f"fs.azure.account.auth.type.{AZStorage.IN_STORAGE_ACCOUNT}.dfs.core.windows.net", "OAuth")
+spark.conf.set(f"fs.azure.account.oauth.provider.type.{AZStorage.IN_STORAGE_ACCOUNT}.dfs.core.windows.net", "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider")
+spark.conf.set(f"fs.azure.account.oauth2.client.id.{AZStorage.IN_STORAGE_ACCOUNT}.dfs.core.windows.net", f"{AZStorage.IN_CLIENT_ID}")
+spark.conf.set(f"fs.azure.account.oauth2.client.secret.{AZStorage.IN_STORAGE_ACCOUNT}.dfs.core.windows.net", f"{AZStorage.IN_CLIENT_SECRET}")
+spark.conf.set(f"fs.azure.account.oauth2.client.endpoint.{AZStorage.IN_STORAGE_ACCOUNT}.dfs.core.windows.net", f"{AZStorage.IN_CLIENT_ENDPOINT}")
+
+spark.conf.set(f"fs.azure.account.auth.type.{AZStorage.OUT_STORAGE_ACCOUNT}.dfs.core.windows.net", "OAuth")
+spark.conf.set(f"fs.azure.account.oauth.provider.type.{AZStorage.OUT_STORAGE_ACCOUNT}.dfs.core.windows.net", "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider")
+spark.conf.set(f"fs.azure.account.oauth2.client.id.{AZStorage.OUT_STORAGE_ACCOUNT}.dfs.core.windows.net", f"{AZStorage.OUT_CLIENT_ID}")
+spark.conf.set(f"fs.azure.account.oauth2.client.secret.{AZStorage.OUT_STORAGE_ACCOUNT}.dfs.core.windows.net", f"{AZStorage.OUT_CLIENT_SECRET}")
+spark.conf.set(f"fs.azure.account.oauth2.client.endpoint.{AZStorage.OUT_STORAGE_ACCOUNT}.dfs.core.windows.net", f"{AZStorage.OUT_CLIENT_ENDPOINT}")
+
+# COMMAND ----------
+
+IN_STORAGE_URI = f"abfss://{AZStorage.IN_CONTAINER}@{AZStorage.IN_STORAGE_ACCOUNT}.dfs.core.windows.net"
+OUT_STORAGE_URI = f"abfss://{AZStorage.OUT_CONTAINER}@{AZStorage.OUT_STORAGE_ACCOUNT}.dfs.core.windows.net"
+
+# COMMAND ----------
+
+hotel_weather_raw = spark.read.format("parquet").load(f"{IN_STORAGE_URI}/hotel-weather")
+
+# COMMAND ----------
+
+
