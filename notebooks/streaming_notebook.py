5c243f9362b2e914a0f734f41985314d430da614
Kirill_Kabanov@epam.com <Kirill_Kabanov@epam.com>
2021-10-13 11:03:49 +0000

    Removed batch logic

--- notebooks/streaming_notebook.py
+++ notebooks/streaming_notebook.py
@@ -42,48 +42,6 @@ OUT_STORAGE_URI = f"abfss://{AZStorage.OUT_CONTAINER}@{AZStorage.OUT_STORAGE_ACC
 
 from pyspark.sql import functions as f
 from pyspark.sql.functions import col
-
-# COMMAND ----------
-
-# IMPORTANT: Batch logic
-hotel_weather_raw = spark.read.format("parquet").load(f"{IN_STORAGE_URI}/hotel-weather")
-
-# COMMAND ----------
-
-hotel_weather_cleaned = (
-    hotel_weather_raw
-    .select(col("id"), col("address"), col("country"), col("city"), f.to_timestamp(col("wthr_date")).alias("wthr_timestamp"), col("avg_tmpr_c"))
-    .withColumnRenamed("id", "hotel_id") \
-    .withColumnRenamed("avg_tmpr_c", "tmpr_c") \
-    .withColumnRenamed("address", "hotel_name")
-)
-
-display(hotel_weather_cleaned)
-
-# COMMAND ----------
-
-hotels_count_by_city = (
-    hotel_weather_cleaned
-    .groupBy("country", "city", "wthr_timestamp")
-    .agg(f.countDistinct("hotel_id").alias("hotels_count"))
-    .orderBy(f.col("hotels_count").desc())
-)
-
-display(hotels_count_by_city)
-
-# COMMAND ----------
-
-weather_count_by_city = (
-    hotel_weather_cleaned
-    .groupBy("country", "city", "wthr_timestamp")
-    .agg(f.max("tmpr_c").alias("max_tmpr_c"), f.min("tmpr_c").alias("min_tmpr_c"), f.avg("tmpr_c").alias("avg_tmpr_c"))
-)
-
-display(weather_count_by_city)
-
-# COMMAND ----------
-
-# IMPORTANT: Streaming Logic
 from pyspark.sql.types import StructType as S, StructField as F
 from pyspark.sql.types import IntegerType, DoubleType, StringType
 
